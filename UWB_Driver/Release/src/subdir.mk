ad8786b8198fb366726d94e24b8488566adc2e97
rip2git <rip2@tpu.ru>
2018-04-04 22:36:46 +0700

    Release (makefile etc) changes

--- UWB_Driver/Release/src/subdir.mk
+++ UWB_Driver/Release/src/subdir.mk
@@ -4,48 +4,12 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 CPP_SRCS += \
-../src/COMHandler.cpp \
-../src/COMPort.cpp \
-../src/ConfigFW.cpp \
-../src/CrossSleep.cpp \
-../src/IniFiles.cpp \
-../src/Logger.cpp \
-../src/NamedPipe.cpp \
-../src/TON.cpp \
-../src/UserInterface.cpp \
-../src/UserPackFW.cpp \
-../src/UserPackHL.cpp \
-../src/UserPacksConverter.cpp \
 ../src/main.cpp 
 
 OBJS += \
-./src/COMHandler.o \
-./src/COMPort.o \
-./src/ConfigFW.o \
-./src/CrossSleep.o \
-./src/IniFiles.o \
-./src/Logger.o \
-./src/NamedPipe.o \
-./src/TON.o \
-./src/UserInterface.o \
-./src/UserPackFW.o \
-./src/UserPackHL.o \
-./src/UserPacksConverter.o \
 ./src/main.o 
 
 CPP_DEPS += \
-./src/COMHandler.d \
-./src/COMPort.d \
-./src/ConfigFW.d \
-./src/CrossSleep.d \
-./src/IniFiles.d \
-./src/Logger.d \
-./src/NamedPipe.d \
-./src/TON.d \
-./src/UserInterface.d \
-./src/UserPackFW.d \
-./src/UserPackHL.d \
-./src/UserPacksConverter.d \
 ./src/main.d 
 
 
